04cadc9333f17b4c078d99d3d990d2d225588712
Bawasam <bawaadapi2@gmail.com>
2025-03-13 08:27:13 -0700

    0-string

---
+++ 10-substring.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+/** a program to print a particular word */
+
+let str = 'I love coding, especially in javascrpt'
+
+console.log('coding')
---
+++ arrithmetic.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+let a = 4
+let b = 3
+let c = 5
+let d = a + b > c 
+ console.log(d)
+
+ // using var 
+ var ad = 48
+ var ba = 20
+
+ var  h = ad + ba 
+  
+ console.log(h)
+
+
+ 
